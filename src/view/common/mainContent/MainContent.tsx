3139d3538d4581728bb2da41381aedb688197527
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-07-22 10:24:43 +0530

    Created the MainContent.tsx to define the routes

---
+++ src/view/common/mainContent/MainContent.tsx
@@ -0,0 +1,13 @@
+import './MainContent.css';
+import {Routes} from "react-router-dom";
+
+
+export function MainContent() {
+    return (
+        <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+            <Routes>
+
+            </Routes>
+        </div>
+    );
+}
