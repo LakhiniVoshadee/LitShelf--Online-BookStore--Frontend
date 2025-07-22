b381d2b654f09e582fc88fa46ac3f4fa8b57b0f1
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-07-22 10:41:04 +0530

    Wrap the App.tsx with provider and provide the redux-store (to define the created store

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css';
+
+import {Route, Routes, useNavigate} from "react-router-dom";
+
+import {DefaultLayout} from "./view/common/DefaultLayout/DefaultLayout.tsx";
+import {useEffect} from "react";
+import {isTokenExpired} from "./auth/auth.ts";
+import {Login} from "./pages/login/Login.tsx";
+
+
+function App() {
+    const navigate = useNavigate(); // for navigation
+
+    useEffect(() => {  // to redirect to login page if no token is present in local storage
+        const token = localStorage.getItem("token");
+        if (!token || isTokenExpired(token)) {
+            localStorage.removeItem("token");
+            navigate("/login");
+        }
+    }, [navigate]);
+    return (
+
+
+        <>
+            <Routes>
+                <Route path="/*" element={<DefaultLayout/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+            </Routes>
+        </>
+
+    );
+}
+
+export default App;
